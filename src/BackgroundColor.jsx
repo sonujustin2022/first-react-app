f8989101e5879a7fbc3a05b21a9ed57eeaaa526c
sonujustin2022 <sonujustin6282@gmail.com>
2026-04-29 23:29:44 +0530

    backGround color finshed

--- src/BackgroundColor.jsx
+++ src/BackgroundColor.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const BackgroundColor = () => {
+
+    const [color,setColor] = useState('white')
+
   return (
-    <div>BackgroundColor</div>
+    <div  style={{height:"100vh", backgroundColor:color}} className='d-flex justify-content-center  gap-3'>
+        <div>
+            <button onClick={()=>setColor('red')} className='btn btn-danger'>Red</button>
+        </div>
+        <div>
+            <button onClick={()=>setColor('blue')} className='btn btn-primary'>Blue</button>
+        </div>
+        <div>
+            <button  onClick={()=>setColor('green')}className='btn btn-success'>Green</button>
+        </div>
+        <div>
+            <button onClick={()=>setColor("yellow")} className='btn btn-warning'>Yellow</button>
+        </div>
+    </div>
   )
 }
 
